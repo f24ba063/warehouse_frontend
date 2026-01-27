75314f0c719d7fba76c9e896973c2a4a914f4631
f24ba063 <f24ba063@chuo.ac.jp>
2026-01-28 08:25:33 +0900

    'モックアップ作成'

--- src/pages/InventoryList/InventoryList.jsx
+++ src/pages/InventoryList/InventoryList.jsx
@@ -1,12 +1,32 @@
 ﻿
+
+import { useState } from 'react';
+
 import '../../css/inventoryList.css';
+
 export default function InventoryList() {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <>
             <div className="inventory-header">
                 <h3 className="header-item gold">新規商品追加</h3>
                 <h3 className="header-item black">在庫情報</h3>
-                <h3 className="header-item black">入庫/出庫</h3>
+
+                <div className="dropdown-wrapper">
+                    <h3
+                        className="header-item black"
+                        onClick={() =>setOpen(!open) }
+                    >
+                        入庫/出庫
+                    </h3>
+
+                    <div className={`dropdown-menu ${open ? 'open' : ''}`}>
+                        <div className="dropdown-item">入庫</div>
+                        <div className="dropdown-item">出庫</div>
+                    </div>
+                </div>
                 <h3 className="header-item black">管理データ</h3>
             </div>
         </>
